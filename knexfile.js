5de8802551a3b6cc46c5af642a4b1b7249a89cec
Michael Rockingham <mrockingham2@gmail.com>
2020-08-28 13:51:31 -0400

    sql test

--- knexfile.js
+++ knexfile.js
@@ -4,22 +4,22 @@ const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhos
 module.exports = {
 
   development: {
-    client: 'pg',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      host: 'localhost',
-      database: 'dads',
-      user:     'postgres',
-      password: 'battlezon2'
+      filename: "./data/dads.db3",
     },
-  
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds",
     },
-    
   },
 production: {
     client: 'pg',
